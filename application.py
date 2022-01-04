8e5616ebcd14accc44971db7dd9beef7dfbf0a30
Evans Ding <evansding@gmail.com>
2022-01-03 18:03:40 -0800

    connected with discord

--- application.py
+++ application.py
@@ -6,5 +6,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+#print(GUILD)
+
 client = discord.Client()
-print(client)
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+
+      if guild.name == GUILD:
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
+
+
+client.run(TOKEN)
+
+
+
+  #@client.event
+  #async def on_message(message):
+    #if message.author == client.user:
+      #return
