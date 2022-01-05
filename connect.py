18a4f7555cb04b613b1a86e9e35fec724de7bdd8
Evans Ding <evansding@gmail.com>
2022-01-04 18:01:29 -0800

    added some bot commands

--- connect.py
+++ connect.py
@@ -11,32 +11,18 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-bot = commands.Bot(command_prefix="$")
-#print(bot)
-
-@bot.command()
-async def ping(ctx):
-  await ctx.channel.send('pong')
-
-#print(GUILD)
-
-#client = discord.Client()
-@bot.event
+client = discord.Client()
+@client.event
 async def on_ready():
-    for guild in bot.guilds:
+    for guild in client.guilds:
 
       if guild.name == GUILD:
         print(
-            f'{bot.user} is connected to the following guild:\n'
+            f'{client.user} is connected to the following guild:\n'
             f'{guild.name}(id: {guild.id})'
         )
 
-@bot.event
-async def on_typing(channel, user, when):
-  #print(user.id)
-  await channel.send('hello there you are typing')
-
-bot.run(TOKEN)
+client.run(TOKEN)
 
 
 
