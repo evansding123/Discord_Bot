096c26ebf257609827a626d7ba7c4ef63bbbe843
Evans Ding <evansding@gmail.com>
2022-01-05 18:33:29 -0800

    added more functions

--- application.py
+++ application.py
@@ -19,8 +19,11 @@ GUILD = os.getenv('DISCORD_GUILD')
 bot = commands.Bot(command_prefix="$")
 #print(bot)
 
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+
 @bot.command()
-async def ping(ctx):
+async def ping(ctx, args):
   await ctx.channel.send('pong')
 
 #print(GUILD)
@@ -36,10 +39,33 @@ async def on_ready():
             f'{guild.name}(id: {guild.id})'
         )
 
+#@bot.event
+#async def on_typing(channel, user, when):
+
+  #await channel.send(f'{user} has chosen {random.choice(Commands.urls)}')
+
 @bot.event
-async def on_typing(channel, user, when):
-  #print(user.id)
-  await channel.send(f'you have chosen {random.choice(Commands.urls)}')
+async def on_message(message):
+  if message.author == bot.user:
+    return
+  if message.content.startswith(f'$hello'):
+    await message.channel.send(f'Hello {message.author}')
+
+@bot.event
+async def on_member_update(before, after):
+  print(before.status)
+  if str(before.status == 'online'):
+    if(str(after.status == 'offline')):
+
+
+      await bot.channel.send(f'{before.name} changed status to {after.status}')
+
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
 
 bot.run(TOKEN)
 
--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
 num1 = input('enter a number: ')
 
 num2 = input('enter another number: ')
+
+
+print(num1, num2)
