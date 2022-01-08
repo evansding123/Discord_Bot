907e28b84a03c8b5387e67b633f5fde4bf9e169f
Evans Ding <evansding@gmail.com>
2022-01-07 18:35:29 -0800

    updated dependencies

--- search.py
+++ search.py
@@ -1,9 +1,10 @@
-#import requests
-#from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
 
 #need to install modules
 
+class SearchYouTube:
+  def __init__(self):
+    self.url = 'https://youtube.com/watch?'
 
-message = '$hello there sire evenas'
 
-print(message.split()[1:])
